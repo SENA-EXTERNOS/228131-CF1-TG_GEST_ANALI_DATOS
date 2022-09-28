ee255d264c084aaec728d50720b9d9a8af51c9f4
harold caicedo <haroldo_89@hotmail.com>
2022-09-29 00:54:40 +0200

    cambio de colores en variables.sass, y actualizacion imagen en introduccion.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,17 +11,17 @@
 
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
       
-      .col-12.col-md-8.col-lg-9
+      .col-12.col-md-8.col-lg-8
         p.mb-4 Estimado aprendiz, reciba una cordial bienvenida a este recurso de aprendizaje. Durante su desarrollo se abordarán los principios básicos de programación, los fundamentos de la lógica de programación, los paradigmas y estándares de programación. Podrá desarrollar desde programas sencillos hasta sofisticados códigos; posteriormente se aprenderá a manejar estructuras concretas: variables, tipos de datos, instrucciones de decisión, de repetición, entre otras. Finalmente, aprenderá a programar en un entorno de desarrollo en línea Google Colab que permite ejecutar código escrito en lenguaje Python una herramienta esencial para crear soluciones de software que le servirán de insumo para adentrarse en el campo de la analítica de datos.
         
         p.mb-4 A continuación, en el video introductorio podrá ver de forma ampliada las diferentes temáticas a desarrollar a lo largo del componente formativo.
 
         p.mb-4 ¡Le deseamos una experiencia de aprendizaje significativa y memorable!
 
-      .col-6.col-md-4.col-lg-3
+      .col-12.col-md-4.col-lg-4
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/falso-verdadero.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/curso/temas/introduccion.svg", alt="Texto que describa la imagen")
 
 </template>
 
