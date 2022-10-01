60abb0da086a0f5810873ef0931d72d64cc57207
harold caicedo <haroldo_89@hotmail.com>
2022-10-01 18:38:27 +0200

    implementacion tema 1

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -15,15 +15,150 @@
       .titulo-secundario__numero
       h2 1.1 Pensamiento computacional
         .col-md-10.align-center
-          text-center.h-100
-            img(src="@/assets/curso/temas/tema1/pensamiento_computacional.svg")
+          img(src="@/assets/curso/temas/tema1/pensamiento_computacional.svg")
 
       br
               
       .col-12
        p El pensamiento computacional fomenta las habilidades que tienen las personas para resolver problemas, ya que, este tipo de pensamiento implica descomponer un problema en unidades más pequeñas e identificar los patrones y características comunes de estas partes, para luego enfocarse en la información relevante y finalmente, solucionar el problema paso a paso a través de algoritmos.
    
-      
+      p.center.align-items-center.align-items-lg-stretch Figura 1. Pensamiento computacional.
+
+        .col-12.col-md-10.align-center
+              img(src="@/assets/curso/temas/tema1/figura_1.svg")
+      br
+
+      .titulo-secundario.color-acento-contenido
+        .titulo-secundario__numero
+        h2 1.2 Algoritmos e instrucciones
+        .col-md-10.align-center
+      br
+
+      p A continuación, se abordarán los conceptos de algoritmos y sus tipos:
+
+      br
+
+    .tarjeta.tarjeta--azul.p-4
+      SlyderA(tipo="b")
+        .row
+          .col-md-6.mb-4.mb-md-0
+            p Un algoritmo en términos generales es una secuencia de pasos que se deben ejecutar en un orden predefinido, con el fin de realizar una tarea específica o resolver un problema en particular. Un claro ejemplo de los algoritmos, son los manuales, porque en ellos encontramos los pasos para resolver los problemas, como los pasos para armar un artefacto o qué hacer en caso de que se presente algún inconveniente o falla con un dispositivo.
+          .col-md-6
+            figure
+              img(src='', alt='Profesor con alumnos')
+        .row.justify-content-center.align-items-center.my-lg-4
+          .col-md-6.mb-4.mb-md-0
+            h6 Inicio y fin
+            br
+            p Una característica de los algoritmos es que los pasos que lo conforman deben ser finitos y contar con un principio y un final..
+          .col-md-6
+            figure
+              img(src='', alt='Profesora y tablero')
+        .row.justify-content-center.align-items-center.my-lg-4
+          .col-md-6.mb-4.mb-md-0
+            h6 Ejemplo de un algoritmo en la vida cotidiana
+            br
+            p nombre del algoritmo: realizar una llamada. Paso 1. Encender el teléfono.Paso 2. Marcar el número del destino. Paso 3. Si contestan: 
+            p Saludar, dialogar con la persona, despedirse Si no contestan: Volver al paso 2. Paso 4. Colgar la llamada.
+          .col-md-6
+            figure
+              img(src='', alt='Joven con un telefono')
+        .row.justify-content-center.align-items-center.my-lg-4
+          .col-md-6.mb-4.mb-md-0
+            br
+            p Como se muestra en el ejemplo anterior, los algoritmos están presentes en muchas situaciones de la vida cotidiana y no son solamente fórmulas matemáticas complejas.  
+          .col-md-6
+            figure
+              img(src='', alt='Joven con un tablero')
+        .row.justify-content-center.align-items-center.my-lg-4
+          .col-md-6.mb-4.mb-md-0
+            h6 Ejemplo 2: una receta de cocina.
+            br
+            p Para mayor claridad respecto al concepto de algoritmos, se revisará otro ejemplo clásico donde se describen los pasos para preparar una comida:
+          .col-md-6
+            figure
+              img(src='', alt='Receta ade cocina')
+        .row.justify-content-center.align-items-center.my-lg-4
+          .col-md-6.mb-4.mb-md-0
+            h6 Algoritmos = Programación
+            br
+            p Ahora bien, los algoritmos se asocian con la programación porque para crear un sistema o aplicación como juegos o software, se necesitan los pasos ordenados y predefinidos para crear dicho sistema. 
+            br
+            p Los algoritmos son el núcleo, la base de los programas.
+          .col-md-6
+            figure
+              img(src='', alt='imagen de PC')
+        .row.justify-content-center.align-items-center.my-lg-4
+          .col-md-6.mb-4.mb-md-0
+            h6 Lenguajes y algoritmos
+            br
+            p Para que la máquina (computador o dispositivo) entienda los pasos o instrucciones que contienen los algoritmos, se requiere de un lenguaje de programación que traduce el algoritmo en órdenes a la máquina. 
+            br
+            p Es importante resaltar que los algoritmos son independientes de los lenguajes de programación, ya que, un mismo algoritmo puede ser traducido a diferentes lenguajes.
+          .col-md-6
+            figure
+              img(src='', alt='computador')
+        .row.justify-content-center.align-items-center.my-lg-4
+          .col-md-6.mb-4.mb-md-0
+            h6 Tipos de algoritmos
+            br
+            p Los algoritmos pueden ser gráficos y no gráficos: los algoritmos gráficos son los que representan las operaciones con imágenes, por ejemplo, los diagramas de flujo. Por otra parte, los algoritmos no gráficos tratan las operaciones a través de una forma descriptiva, y entre ellos tenemos al pseudocódigo. 
+            br
+            p A continuación, se trabajarán ambos tipos de algoritmos de forma más detallada.
+          .col-md-6
+            figure
+              img(src='', alt='pseudocodigo vs diagrama de flujo')
+            br
+
+    .row.pt-4
+      .titulo-segundo.color-primario
+        h2 1.3 Diagramas de flujo
+
+    .row.pt-4.pb-4 
+      .col-12
+        .row.align-middle 
+          .col-12.col-md-3.col-lg-3
+            img(src="@/assets/curso/temas/tema1/diagrama_flujo.svg", alt="alt").img100
+          .col-12.col-md-9.col-lg-9
+              p.pb-2 Los diagramas de flujo son representaciones gráficas de los algoritmos para representar de manera clara, puntual y organizada la información que se desea tratar. También son llamados flujogramas.
+              p Los flujogramas cuentan con una simbología establecida por el Instituto Nacional Estadounidense de Estándares (ANSI). A continuación, se relacionan los símbolos más usados:
+
+    .row.pt-4.pb-4 
+      .col-12
+        .row.align-middle 
+          .col-12.col-md-4.col-lg-4
+            img(src="@/assets/curso/temas/tema1/diagrama_1.svg", alt="alt").img100
+            figcaption Símbolo para Iniciar y Finalizar
+          .col-12.col-md-4.col-lg-4
+            img(src="@/assets/curso/temas/tema1/diagrama_2.svg", alt="alt").img100
+            figcaption Símbolo para realizar una acción o proceso
+          .col-12.col-md-4.col-lg-4
+            img(src="@/assets/curso/temas/tema1/diagrama_3.svg", alt="alt").img100
+            figcaption Símbolo de decisión
+
+    .row.justify-content-center
+      .col-12
+        .row.align-middle 
+          .col-12.col-md-4.col-lg-4
+            img(src="@/assets/curso/temas/tema1/diagrama_4.svg", alt="alt").img100
+            figcaption Símbolo para las entradas y salidas
+          .col-12.col-md-4.col-lg-4
+            img(src="@/assets/curso/temas/tema1/diagrama_5.svg", alt="alt").img100
+            figcaption Símbolo para conectar
+
+    .row.justify-content-center.pt-5
+      .col-8
+        .row.align-middle
+          .titulo-sexto.color-secundario.mb-2.pr-2
+            h5 Tabla 3.
+            span Lenguajes compilados Vs. interpretados
+        .row
+          .col-12
+            img(src="@/assets/curso/temas/tema1/diagrama_ejemplo.svg", alt="alt").img100
+            figcaption Símbolo para las entradas y salidas
+
+
+
 </template>
 
 <script>
