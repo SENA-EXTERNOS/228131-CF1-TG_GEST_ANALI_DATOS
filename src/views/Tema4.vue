874476d018ddde7b6530cc4fcba7876b0d94523b
harold caicedo <haroldo_89@hotmail.com>
2022-10-04 23:42:35 -0500

    tema 4. estructuras

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -34,7 +34,43 @@
                   .tarjeta.h-50.d-flex.p-4.align-middle
                     figure
                       img(src="@/assets/curso/temas/tema4/paradigma_imperativo.svg", alt="paradigma_imperativo").img100
+            .tarjeta.clr--gris(titulo="Paradigma declarativo")
+              .row.tarjeta.tarjeta--azul.align-middle 
+                .col-12.col-md-8.col-lg-8
+                  .tarjeta.d-flex.p-4.align-middle
+                    p.b.mb-4 Paradigma declarativo
+                    p.mb-4 El desarrollador declara las condiciones o restricciones que describen la problemática que se quiere resolver, pero no los pasos necesarios para llegar a la solución.
+                    p.mb-4 Lenguajes como Haskell, Miranda y SQL hacen parte de este paradigma.
+                .col-12.col-md-4.col-lg-4
+                  .tarjeta.h-50.d-flex.p-4.align-middle
+                    figure
+                      img(src="@/assets/curso/temas/tema4/paradigma_declarativo.svg", alt="paradigma_declarativo").img100
+            .tarjeta.clr--gris(titulo="Paradigma orientado a objetos")
+              .row.tarjeta.tarjeta--azul.align-middle 
+                .col-12.col-md-8.col-lg-8
+                  .tarjeta.d-flex.p-4.align-middle
+                    p.b.mb-4 Paradigma orientado a objetos
+                    p.mb-4 El desarrollador construye “objetos” que representan a cada uno de los elementos que hacen parte del problema que se va a resolver. Estos objetos tienen características y funciones que permiten minimizar su creación y mantenimiento. Además, se disminuyen los errores y permiten la reutilización de código.
+                    p.mb-4 Entre los lenguajes representativos de este paradigma están: Java, Python y C# entre otros.
+                .col-12.col-md-4.col-lg-4
+                  .tarjeta.h-50.d-flex.p-4.align-middle
+                    figure
+                      img(src="@/assets/curso/temas/tema4/object_oriented.svg", alt="paradigma_orientado_objetos").img100
 
+    #t_4_1.titulo-segundo.color-primario.pt.3.pb-3
+      h2 4.1  Programación estructurada
+    .row.justify-content-center.pt-5
+      .col-12
+        p La programación estructurada hace parte del paradigma imperativo y está diseñado para promover la claridad, calidad y el tiempo de desarrollo de las aplicaciones, utilizando las siguientes estructuras:   
+    .row.justify-content-center.pt-5
+      .col-8
+        .row.align-middle
+          .titulo-sexto.color-secundario.mb-2.pr-2
+            h5 Figura 22.
+            span Tipos de estructuras
+        .row
+          .col-12
+            img(src="@/assets/curso/temas/tema4/estructuras.svg", alt="estructuras").img100
 </template>
 
 <script>
