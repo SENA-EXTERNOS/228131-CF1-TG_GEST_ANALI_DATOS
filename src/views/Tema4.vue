6497cd4447b6782a558e8a704a0dc449b36c08a4
harold caicedo <haroldo_89@hotmail.com>
2022-10-04 23:13:00 -0500

    Update Tema4.vue

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -23,15 +23,17 @@
       .row
         .col-lg-12.order-2.order-lg-1
           TabsA.color-acento-botones       
-            .tarjeta.clr--gris(titulo="Cantidad de lenguajes soportados:")
+            .tarjeta.clr--gris(titulo="Paradigma imperativo")
               .row.tarjeta.tarjeta--azul.align-middle 
                 .col-12.col-md-8.col-lg-8
                   .tarjeta.d-flex.p-4.align-middle
-                    p.mb-4 Cantidad de lenguajes soportados: existen IDE que solamente son compatibles con un lenguaje de programación, por lo tanto, son mejores para un estilo de programación en especial, entre ellos, tenemos como ejemplo a IntellinJ, que se conoce como un IDE exclusivo para Java. Por otra parte, están los IDE que soportan varios lenguajes de programación, como es el caso de Eclipse, con el que se puede programar en Java, Python, JavaScript, PHP, C++, entre otros.
+                    p.b.pb-3 Paradigma imperativo
+                    p.mb-4 El desarrollador describe las instrucciones u órdenes y la máquina las ejecuta paso a paso. 
+                    p.mb-4 Algunos lenguajes de programación de este paradigma son: Pascal, Python, C, C++, Basic, entre otros.
                 .col-12.col-md-4.col-lg-4
                   .tarjeta.h-50.d-flex.p-4.align-middle
                     figure
-                      img(src="@/assets/curso/temas/tema2/tipo_ide_1.svg", alt="alt").img100
+                      img(src="@/assets/curso/temas/tema4/paradigma_imperativo.svg", alt="paradigma_imperativo").img100
 
 </template>
 
