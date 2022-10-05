fa339d77361e84a44d8523c7f8a62c6c407b6745
harold caicedo <haroldo_89@hotmail.com>
2022-10-04 22:33:21 -0500

    Create Tema4.vue

---
+++ src/views/Tema4.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 4
+      h1 Paradigmas y estándares de programación
+
+      .row.justify-content-center.pt-5
+        .col-10
+          .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5 
+            .row.m-0.align-items-center.justify-content-between
+              .col-lg-8
+                .bloque-texto-a__texto.p-4
+                  p.mb-0 Para Adell et al. (2019) el pensamiento computacional “Implica resolver problemas, diseñar sistemas y comprender el comportamiento humano, basándose en los conceptos fundamentales de la ciencia de la computación” (pg. 3).  
+              .col-lg-4.mb-4.mb-lg-0(data-aos="fade-left")
+                figure
+                  img(src="@/assets/curso/temas/tema1/pensamiento_computacional.svg").img100
