53bcd685bdf54a7c76e6f6f49d4841c05f77420c
harold.caicedo <harold.caicedo>
2022-11-01 12:48:56 +0100

    se integra actividad didactiva al componente

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .curso-main-container.pb-3
-  BannerInterno(icono="fas fa-tasks" titulo="Actividad didáctica")
+  BannerInterno(icono="fas fa-tasks" titulo="Fundamentos y lógica de programación")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
@@ -11,7 +11,7 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/falso-verdadero.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/falso-verdadero.svg", alt="falso-verdadero")
       
       .col-12.col-md-8.col-lg-9
         p.mb-4 Esta actividad interactiva, tiene por finalidad revisar los conceptos fundamentales de programación para la analítica de datos.
@@ -20,7 +20,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Falso / verdadero
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
